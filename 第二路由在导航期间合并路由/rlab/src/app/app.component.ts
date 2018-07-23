0388a23da30362f5144090717e635e07a1315a3b
y1笑而过 <15335747148@163.com>
2018-07-23 23:29:15 +0800

    第二路由在导航期间合并路由
    第二路由在导航期间合并路由

---
+++ 第二路由在导航期间合并路由/rlab/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+/**
+ * 第二路由导航：在导航期间合并路由
+ * @see CrisisCenterComponent
+ */
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+}
